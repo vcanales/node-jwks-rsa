4b64f234e7f96f716a2fc43d5032d3aafabae8d2
Ethan Arrowood <ethan.arrowood@gmail.com>
2018-01-16 00:43:09 -0500

    add fastify integration

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { JwksClient } from './JwksClient';
 import * as errors from './errors';
 import { hapiJwt2Key, hapiJwt2KeyAsync } from './integrations/hapi';
 import { expressJwtSecret } from './integrations/express';
+import { fastifyJwtSecret } from './integrations/fastify';
+import { hapiJwt2Key } from './integrations/hapi';
 import { koaJwtSecret } from './integrations/koa';
 
 module.exports = (options) => {
@@ -15,6 +17,7 @@ module.exports.JwksRateLimitError = errors.JwksRateLimitError;
 module.exports.SigningKeyNotFoundError = errors.SigningKeyNotFoundError;
 
 module.exports.expressJwtSecret = expressJwtSecret;
+module.exports.fastifyJwtSecret = fastifyJwtSecret;
 module.exports.hapiJwt2Key = hapiJwt2Key;
 module.exports.hapiJwt2KeyAsync = hapiJwt2KeyAsync;
 module.exports.koaJwtSecret = koaJwtSecret;
